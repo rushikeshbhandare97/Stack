87b24f2bb9d9327aedfda2c27542e1294bf6b8d4
technology-97 <rushikeshbhandare789@gmail.com>
2021-08-01 23:27:29 +0600

    Added:UC3_EnqueueStack  program is Added

---
+++ Queue.java
@@ -0,0 +1,101 @@
+// Queue implementation in Java
+
+    public class Queue {
+        int SIZE = 5;
+        int items[] = new int[SIZE];
+        int front, rear;
+
+        Queue() {
+            front = -1;
+            rear = -1;
+        }
+
+        boolean isFull() {
+            if (front == 0 && rear == SIZE - 1) {
+                return true;
+            }
+            return false;
+        }
+
+        boolean isEmpty() {
+            if (front == -1)
+                return true;
+            else
+                return false;
+        }
+
+        void enQueue(int element) {
+            if (isFull()) {
+                System.out.println("Queue is full");
+            } else {
+                if (front == -1)
+                    front = 0;
+                rear++;
+                items[rear] = element;
+                System.out.println("Inserted " + element);
+            }
+        }
+
+        int deQueue() {
+            int element;
+            if (isEmpty()) {
+                System.out.println("Queue is empty");
+                return (-1);
+            } else {
+                element = items[front];
+                if (front >= rear) {
+                    front = -1;
+                    rear = -1;
+                } /* Q has only one element, so we reset the queue after deleting it. */
+                else {
+                    front++;
+                }
+                System.out.println("Deleted -> " + element);
+                return (element);
+            }
+        }
+
+        void display() {
+            /* Function to display elements of Queue */
+            int i;
+            if (isEmpty()) {
+                System.out.println("Empty Queue");
+            } else {
+                System.out.println("\nFront index-> " + front);
+                System.out.println("Items -> ");
+                for (i = front; i <= rear; i++)
+                    System.out.print(items[i] + "  ");
+
+                System.out.println("\nRear index-> " + rear);
+            }
+        }
+
+        public static void main(String[] args) {
+            Queue q = new Queue();
+
+            // deQueue is not possible on empty queue
+            q.deQueue();
+
+            // enQueue 5 elements
+            q.enQueue(56);
+            q.enQueue(30);
+            q.enQueue(70);
+
+
+
+
+            q.display();
+
+
+            // Now we have just 4 elements
+            q.display();
+
+        }
+    }
+
+
+
+
+
+
+
